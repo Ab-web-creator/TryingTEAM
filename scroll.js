07e77dca4992eb9d3e465390a9b79f04f2dc8702
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-08-07 22:31:54 +0200

    menshiy shrift

--- scroll.js
+++ scroll.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-    const EFFECT = document.querySelector("#effect");
+    const EFFECT = document.querySelectorId("#effect");
     window.addEventListener('scroll', scrollEffect );
 
     function scrollEffect() {
         if(window.scrollY >= 500) {
             EFFECT.style.opacity = '1';
             EFFECT.style.transform = 'translateX(0px)';
-            EFFECT.style.transition = '5s ease-in-out';
+            EFFECT.style.transition = 5s ease-in-out';
         }
         else {
             EFFECT.style.opacity = '0.4';
