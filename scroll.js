02ca7830e96067a1d0bb821fce9b2305c0286e9e
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-08-07 23:16:17 +0200

    lyuboe sobwenie

--- scroll.js
+++ scroll.js
@@ -1,17 +1,14 @@
-window.onload = function() {
-    const EFFECT = document.querySelectorId("#effect");
-    window.addEventListener('scroll', scrollEffect );
+const cards = document.querySelectorAll('.card')
 
-    function scrollEffect() {
-        if(window.scrollY >= 500) {
-            EFFECT.style.opacity = '1';
-            EFFECT.style.transform = 'translateX(0px)';
-            EFFECT.style.transition = 5s ease-in-out';
-        }
-        else {
-            EFFECT.style.opacity = '0.4';
-            EFFECT.style.transform = 'translateX(-50px)';
-        }
+const callback = (elements) => {
+  elements.forEach((ele) => {
+    if (ele.isIntersecting && !ele.target.classList.contains('show')) {
+      ele.target.classList.add('show')
+    } else {
+      ele.target.classList.remove('show')
     }
-    scrollEffect();
+  })
 }
+
+const observer = new IntersectionObserver(callback)
+cards.forEach((card) => observer.observe(card))
