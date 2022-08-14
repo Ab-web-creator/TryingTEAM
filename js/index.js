55972558e6e918559aafe1b0658d42dff16898d7
atabek-bakh <coderonline@icloud.com>
2022-08-14 22:11:07 +0200

    files

--- js/index.js
+++ js/index.js
@@ -1,22 +1,61 @@
-const sections = document.querySelectorAll("section");
-
+const sections = document.querySelectorAll('.logo')
+console.log(sections)
 const options = {
-    root: null,
-    threshold: 0,
-    rootMargin: "-150px"
-};
+  root: null,
+  threshold: 0,
+  rootMargin: '0px',
+}
 
 const observer = new IntersectionObserver(function (entries, observer) {
-    entries.forEach(entry => {
-       if (!entry.isIntersecting) {
-        return;
-       } 
-       console.log(entry);
-       entry.target.classList.toggle("inverse");
-    })
-}, options);
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return
+    }
+    console.log(entry)
+    if (entry.target.classList.contains('inverse')) {
+      entry.target.classList.remove('inverse')
+      setTimeout(() => {
+        entry.target.classList.add('inverse')
+      }, 1000)
+    } else {
+      setTimeout(() => {
+        entry.target.classList.add('inverse')
+      }, 500)
+    }
+  })
+}, options)
 
+sections.forEach((section) => {
+  observer.observe(section)
+})
 
-sections.forEach(section => {
-    observer.observe(section);
+const blueContainers = document.querySelectorAll('.blue_container')
+console.log(blueContainers)
+const settings = {
+  root: null,
+  threshold: 0,
+  rootMargin: '0px',
+}
+
+const watcher = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return
+    }
+    console.log(entry)
+    if (entry.target.classList.contains('inverse')) {
+      entry.target.classList.remove('inverse')
+      setTimeout(() => {
+        entry.target.classList.add('inverse')
+      }, 1000)
+    } else {
+      setTimeout(() => {
+        entry.target.classList.add('inverse')
+      }, 500)
+    }
+  })
+}, settings)
+
+blueContainers.forEach((section) => {
+  watcher.observe(section)
 })
