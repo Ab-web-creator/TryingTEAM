a4ba5374bf7574bca627d9c57ab539f351648cb3
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-08-15 21:39:21 +0200

    lyuboe sobwenie

--- js/index.js
+++ js/index.js
@@ -1,61 +1,79 @@
-const sections = document.querySelectorAll('.logo')
-console.log(sections)
-const options = {
-  root: null,
-  threshold: 0,
-  rootMargin: '0px',
-}
-
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return
-    }
-    console.log(entry)
-    if (entry.target.classList.contains('inverse')) {
-      entry.target.classList.remove('inverse')
-      setTimeout(() => {
-        entry.target.classList.add('inverse')
-      }, 1000)
-    } else {
-      setTimeout(() => {
-        entry.target.classList.add('inverse')
-      }, 500)
-    }
-  })
-}, options)
-
-sections.forEach((section) => {
-  observer.observe(section)
-})
-
+const logos = document.querySelectorAll('.logo')
 const blueContainers = document.querySelectorAll('.blue_container')
-console.log(blueContainers)
-const settings = {
-  root: null,
-  threshold: 0,
-  rootMargin: '0px',
+
+function scrollHandler(e) {
+  var atSnappingPoint = e.target.scrollLeft % e.target.offsetWidth === 0
+  var timeOut = atSnappingPoint ? 0 : 150 //see notes
+
+  clearTimeout(e.target.scrollTimeout) //clear previous timeout
+
+  e.target.scrollTimeout = setTimeout(function () {
+    console.log('Scrolling stopped!')
+    const sections = document.querySelectorAll('.logo')
+    console.log(sections)
+    const options = {
+      root: null,
+      threshold: 0,
+      rootMargin: '0px',
+    }
+
+    const observer = new IntersectionObserver(function (entries, observer) {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          return
+        }
+        console.log(entry)
+        if (entry.target.classList.contains('inverse')) {
+          entry.target.classList.remove('inverse')
+          setTimeout(() => {
+            entry.target.classList.add('inverse')
+          }, 1000)
+        } else {
+          setTimeout(() => {
+            entry.target.classList.add('inverse')
+          }, 500)
+        }
+      })
+    }, options)
+
+    sections.forEach((section) => {
+      observer.observe(section)
+    })
+
+    console.log(blueContainers)
+    const settings = {
+      root: null,
+      threshold: 0,
+      rootMargin: '0px',
+    }
+
+    const watcher = new IntersectionObserver(function (entries, observer) {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          return
+        }
+        console.log(entry)
+        if (entry.target.classList.contains('inverse')) {
+          entry.target.classList.remove('inverse')
+          setTimeout(() => {
+            entry.target.classList.add('inverse')
+          }, 1000)
+        } else {
+          setTimeout(() => {
+            entry.target.classList.add('inverse')
+          }, 500)
+        }
+      })
+    }, settings)
+
+    blueContainers.forEach((section) => {
+      watcher.observe(section)
+    })
+  }, timeOut)
 }
 
-const watcher = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return
-    }
-    console.log(entry)
-    if (entry.target.classList.contains('inverse')) {
-      entry.target.classList.remove('inverse')
-      setTimeout(() => {
-        entry.target.classList.add('inverse')
-      }, 1000)
-    } else {
-      setTimeout(() => {
-        entry.target.classList.add('inverse')
-      }, 500)
-    }
-  })
-}, settings)
+const scroller = document.getElementById('scroller')
 
-blueContainers.forEach((section) => {
-  watcher.observe(section)
-})
+scroller.addEventListener('scroll', scrollHandler)
+
+console.log(isScrolling)
