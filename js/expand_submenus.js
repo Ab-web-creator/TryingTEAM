18c81a2311a8db8c24e1d8bd6d4669aa4d6be702
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-08-20 18:36:50 +0200

    lyuboe sobwenie

--- js/expand_submenus.js
+++ js/expand_submenus.js
@@ -1,28 +1,39 @@
 // This js makes SUBmenus expand
 
+$('.feat-btn').click(function () {
+  if ($('.serv-show').hasClass('show')) {
+    $('.serv-show').toggleClass('show')
+  }
+  if ($('.theory-show').hasClass('show')) {
+    $('.theory-show').toggleClass('show')
+  }
+  $('.feat-show').toggleClass('show')
+  $('.firstMenuPackOpener').toggleClass('rotate')
+})
 
-$('.feat-btn').click(function() {
-    $('aside ul .feat-show').toggleClass("show");
-    $('aside ul .firstMenuPackOpener').toggleClass("rotate");
-  });
+$('.serv-btn').click(function () {
+  if ($('.feat-show').hasClass('show')) {
+    $('.feat-show').toggleClass('show')
+  }
+  if ($('.theory-show').hasClass('show')) {
+    $('.theory-show').toggleClass('show')
+  }
+  $('.serv-show').toggleClass('show')
+  $('.secondMenuPackOpener').toggleClass('rotate')
+})
 
-  $('.serv-btn').click(function() {
-    $('aside ul .serv-show').toggleClass("show1");
-    $('aside ul .secondMenuPackOpener').toggleClass("rotate");
-  });
+$('.theory-btn').click(function () {
+  if ($('.feat-show').hasClass('show')) {
+    $('.feat-show').toggleClass('show')
+  }
+  if ($('.serv-show').hasClass('show')) {
+    $('.serv-show').toggleClass('show')
+  }
 
-  $('.theory-btn').click(function() {
-    $('aside ul .theory-show').toggleClass("show2");
-    $('aside ul .thirdMenuPackOpener').toggleClass("rotate");
-  });
+  $('.theory-show').toggleClass('show')
+  $('.thirdMenuPackOpener').toggleClass('rotate')
+})
 
-  $('.aside ul li').click(function() {
-
-    // Вот здесь если напишем код который пройдет forEach и убирает у всех класс (например, "show", "show1" и "show2")
-    $('aside ul .feat-show.show').removeClass("show");
-    $('aside ul .serv-show.show1').removeClass("show1");
-    $('aside ul .theory-show.show2').removeClass("show2");
- 
-
-    $(this).addClass("active").siblings().removeClass("active"); 
-  });
+$('.aside ul li').click(function () {
+  $(this).addClass('active').siblings().removeClass('active')
+})
