a7029567ff040d1e808cec7860d69fb52dfea12e
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-08-24 19:46:29 +0200

    lyuboe sobwenie

---
+++ tesla.js
@@ -0,0 +1,62 @@
+const main = document.querySelector(".main-1");
+const menu = document.querySelector("#menu");
+const cross = document.querySelector(".fa-xmark");
+const blurs = document.querySelector(".blur");
+const side = document.querySelector(".hdn-side");
+
+menu.addEventListener("click", () => {
+    blurs.classList.add("active");
+    side.classList.add("active");
+})
+
+cross.addEventListener("click", () => {
+    blurs.classList.remove("active");
+    side.classList.remove("active");
+})
+
+const h1 = document.querySelector(".head-1");
+const h2 = document.querySelector(".head-2");
+const h3 = document.querySelector(".head-3");
+const h4 = document.querySelector(".head-4");
+
+const l1 = document.querySelector(".login-1");
+const l2 = document.querySelector(".login-2");
+const l3 = document.querySelector(".login-3");
+const l4 = document.querySelector(".login-4");
+
+main.addEventListener("scroll", () => {
+    console.log(main.scrollTop);
+    const unit = main.scrollTop;
+
+    if(unit == 0 && unit < 100) {
+        h1.classList.remove("hide");
+        l1.classList.remove("hide");
+    } else {
+        h1.classList.add("hide");
+        l1.classList.add("hide");
+    }
+
+    if (unit > 600 && unit < 700) {
+        h2.classList.add("active");
+        l2.classList.add("active");
+    } else {
+        h2.classList.remove("active");
+        l2.classList.remove("active");
+    }
+
+    if (unit > 1200 && unit < 1300) {
+        h3.classList.add("active");
+        l3.classList.add("active");
+    } else {
+        h3.classList.remove("active");
+        l3.classList.remove("active");
+    }
+
+    if (unit > 1900 && unit < 2000) {
+        h4.classList.add("active");
+        l4.classList.add("active");
+    } else {
+        h4.classList.remove("active");
+        l4.classList.remove("active");
+    }
+})
