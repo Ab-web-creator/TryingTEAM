bc65866f5c04307c33cecc81ea56551eadd88b40
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-09-10 16:26:38 +0200

    lyuboe sobwenie

--- js/mainknop.js
+++ js/mainknop.js
@@ -1,8 +1,11 @@
+
 // document.getElementById("main_knop_clicker").addEventListener("click", change_gif);
 // function change_gif () {
 //     console.log("dddd");
 //     document.getElementById("bluezones").style.backgroundImage = "url(images/1_start_menu.gif)";
 // }
+
+
 
 const mainKnop1_1 = document.querySelector("#main_knop_clicker1_1");
 const mainKnop1_2 = document.querySelector("#main_knop_clicker1_2");
@@ -15,15 +18,17 @@ const bluezone1 = document.querySelector("#blueZone1");
 
 mainKnop1_1.addEventListener("click", () => {
     console.log("Menu-Big knop 1 is clicked");
-    // bluezone1.style.backgroundImage = "url(images/1_start_menu.gif)";
+
+    bluezone1.style.backgroundImage = "url(images/1_start_menu.gif)";
     mainKnop1_1.style.zIndex = 200;
     mainKnop1_2.style.zIndex = 201;
 })
 
+
 mainKnop1_2.addEventListener("click", () => {
     console.log("Menu-Big knop 2 is clicked");
-    // bluezone1.style.backgroundImage = "url(images/1_stop_menu.gif)";
-    mainKnop1_1.style.zIndex = 200;
+    bluezone1.style.backgroundImage = "url(images/1_stop_menu.gif)";
+    mainKnop1_1.style.zIndex = 1200;
 
     setTimeout(() => {
         bluezone1.style.backgroundImage = "url(images/1_basis.gif)";
