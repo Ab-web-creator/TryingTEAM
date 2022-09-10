b1f1d5e1e9e124ab5c81ba509cb0431e49ce3d87
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-09-10 14:08:31 +0200

    lyuboe sobwenie

--- js/homePageOnly.js
+++ js/homePageOnly.js
@@ -4,10 +4,10 @@ const h2 = document.querySelector('.head-2')
 const h3 = document.querySelector('.head-3')
 const h4 = document.querySelector('.head-4')
 
-const l1 = document.querySelector('.login-1')
-const l2 = document.querySelector('.login-2')
-const l3 = document.querySelector('.login-3')
-const l4 = document.querySelector('.login-4')
+// const l1 = document.querySelector('.login-1')
+// const l2 = document.querySelector('.login-2')
+// const l3 = document.querySelector('.login-3')
+// const l4 = document.querySelector('.login-4')
 
 const page1 = document.getElementById('page-1')
 const page2 = document.getElementById('page-2')
@@ -36,16 +36,16 @@ mainDiv.addEventListener('scroll', () => {
   if (scrolledHeight >= 0 && scrolledHeight <= page1.scrollHeight * 0.2) {
     console.log('Page 1')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
     h4.classList.remove('active')
-    l4.classList.remove('active')
+    // l4.classList.remove('active')
 
     h1.style.opacity = 1
-    l1.style.opacity = 1
+    // l1.style.opacity = 1
     h1.classList.add('active')
-    l1.classList.add('active')
+    // l1.classList.add('active')
   }
 
   if (
@@ -54,14 +54,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('0.4')
     h1.classList.remove('active')
-    l1.classList.remove('active')
+    // l1.classList.remove('active')
     h1.style.opacity = 0.4
-    l1.style.opacity = 0.4
+    // l1.style.opacity = 0.4
 
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h2.style.opacity = 0
-    l2.style.opacity = 0
+    // l2.style.opacity = 0
   }
 
   if (
@@ -70,14 +70,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('0.6')
     h1.classList.remove('active')
-    l1.classList.remove('active')
+    // l1.classList.remove('active')
     h1.style.opacity = 0
-    l1.style.opacity = 0
+    // l1.style.opacity = 0
 
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h2.style.opacity = 0
-    l2.style.opacity = 0
+    // l2.style.opacity = 0
   }
 
   if (
@@ -86,14 +86,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('0.8')
     h1.classList.remove('active')
-    l1.classList.remove('active')
+    // l1.classList.remove('active')
     h1.style.opacity = 0
-    l1.style.opacity = 0
+    // l1.style.opacity = 0
 
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h2.style.opacity = 0.1
-    l2.style.opacity = 0.1
+    // l2.style.opacity = 0.1
   }
 
   if (
@@ -102,14 +102,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('1')
     h1.classList.remove('active')
-    l1.classList.remove('active')
+    // l1.classList.remove('active')
     h1.style.opacity = 0
-    l1.style.opacity = 0
+    // l1.style.opacity = 0
 
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h2.style.opacity = 0.6
-    l2.style.opacity = 0.6
+    // l2.style.opacity = 0.6
   }
 
   if (
@@ -119,18 +119,18 @@ mainDiv.addEventListener('scroll', () => {
     console.log('page 2')
 
     h1.classList.remove('active')
-    l1.classList.remove('active')
+    // l1.classList.remove('active')
 
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
 
     h4.classList.remove('active')
-    l4.classList.remove('active')
+    // l4.classList.remove('active')
 
     h2.style.opacity = 1
-    l2.style.opacity = 1
+    // l2.style.opacity = 1
     h2.classList.add('active')
-    l2.classList.add('active')
+    // l2.classList.add('active')
   }
 
   if (
@@ -139,14 +139,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('0.4')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h2.style.opacity = 0.4
-    l2.style.opacity = 0.4
+    // l2.style.opacity = 0.4
 
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
     h3.style.opacity = 0
-    l3.style.opacity = 0
+    // l3.style.opacity = 0
   }
 
   if (
@@ -155,14 +155,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('0.6')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h2.style.opacity = 0
-    l2.style.opacity = 0
+    // l2.style.opacity = 0
 
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
     h3.style.opacity = 0
-    l3.style.opacity = 0
+    // l3.style.opacity = 0
   }
 
   if (
@@ -171,14 +171,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('0.8')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h2.style.opacity = 0
-    l2.style.opacity = 0
+    // l2.style.opacity = 0
 
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
     h3.style.opacity = 0.1
-    l3.style.opacity = 0.1
+    // l3.style.opacity = 0.1
   }
 
   if (
@@ -187,14 +187,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('1')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h2.style.opacity = 0
-    l2.style.opacity = 0
+    // l2.style.opacity = 0
 
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
     h3.style.opacity = 0.6
-    l3.style.opacity = 0.6
+    // l3.style.opacity = 0.6
   }
 
   if (
@@ -205,18 +205,18 @@ mainDiv.addEventListener('scroll', () => {
     // 500 < 500 + 100) {
     console.log('page 3')
     h1.classList.remove('active')
-    l1.classList.remove('active')
+    // l1.classList.remove('active')
 
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
 
     h4.classList.remove('active')
-    l4.classList.remove('active')
+    // l4.classList.remove('active')
 
     h3.style.opacity = 1
-    l3.style.opacity = 1
+    // l3.style.opacity = 1
     h3.classList.add('active')
-    l3.classList.add('active')
+    // l3.classList.add('active')
   }
 
   if (
@@ -227,14 +227,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('0.4')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h3.style.opacity = 0.4
-    l3.style.opacity = 0.4
+    // l3.style.opacity = 0.4
 
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
     h4.style.opacity = 0
-    l4.style.opacity = 0
+    // l4.style.opacity = 0
   }
 
   if (
@@ -245,12 +245,12 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('0.6')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h3.style.opacity = 0
-    l3.style.opacity = 0
+    // l3.style.opacity = 0
 
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
     h4.style.opacity = 0
     l4.style.opacity = 0
   }
@@ -263,14 +263,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('0.8')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h3.style.opacity = 0
-    l3.style.opacity = 0
+    // l3.style.opacity = 0
 
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
     h4.style.opacity = 0.1
-    l4.style.opacity = 0.1
+    // l4.style.opacity = 0.1
   }
 
   if (
@@ -281,14 +281,14 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('1')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h3.style.opacity = 0
-    l3.style.opacity = 0
+    // l3.style.opacity = 0
 
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
     h4.style.opacity = 0.5
-    l4.style.opacity = 0.5
+    // l4.style.opacity = 0.5
   }
 
   if (
@@ -297,15 +297,15 @@ mainDiv.addEventListener('scroll', () => {
   ) {
     console.log('4')
     h1.classList.remove('active')
-    l1.classList.remove('active')
+    // l1.classList.remove('active')
     h2.classList.remove('active')
-    l2.classList.remove('active')
+    // l2.classList.remove('active')
     h3.classList.remove('active')
-    l3.classList.remove('active')
+    // l3.classList.remove('active')
 
     h4.style.opacity = 1
-    l4.style.opacity = 1
+    // l4.style.opacity = 1
     h4.classList.add('active')
-    l4.classList.add('active')
+    // l4.classList.add('active')
   }
 })
