012dedd49bde392fce4e3ab9c6e216bc059d4aac
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-09-10 10:19:39 +0200

    lyuboe sobwenie

--- js/mainknop.js
+++ js/mainknop.js
@@ -4,44 +4,34 @@
 //     document.getElementById("bluezones").style.backgroundImage = "url(images/1_start_menu.gif)";
 // }
 
-const mainKnop = document.querySelector("#main_knop_clicker");
+const mainKnop1 = document.querySelector("#main_knop_clicker1");
 const mainKnop2 = document.querySelector("#main_knop_clicker2");
 const mainKnop3 = document.querySelector("#main_knop_clicker3");
-const bluezones = document.querySelector("#bluezones");
-
-var image1 = new Image();
-image1.src = "images/1_basis.gif";
-var image2 = new Image();
-image2.src = "images/1_start_menu.gif";
-var image3 = new Image();
-image3.src = "images/1_stop_menu.gif";
+const bluezone2 = document.querySelector("#blueZone2");
 
 
 
-mainKnop.addEventListener("click", () => {
-    console.log("Menu-Big knop is clicked");
-    bluezones.style.backgroundImage = "url(images/1_start_menu.gif)";
-    mainKnop.style.zIndex = 200;
+mainKnop1.addEventListener("click", () => {
+    console.log("Menu-Big knop 1 is clicked");
+    bluezone2.style.backgroundImage = "url(images/1_start_menu.gif)";
+    mainKnop1.style.zIndex = 200;
     mainKnop2.style.zIndex = 201;
-
-    image2.attr('src', image2.src);
 })
 
 mainKnop2.addEventListener("click", () => {
     console.log("Menu-Big knop 2 is clicked");
-    bluezones.style.backgroundImage = "url(images/1_stop_menu.gif)";
+    bluezone2.style.backgroundImage = "url(images/1_stop_menu.gif)";
     mainKnop3.style.zIndex = 201;
     mainKnop2.style.zIndex = 200;
 
-    image3.attr('src', image3.src);
+    
 })
 
 mainKnop3.addEventListener("click", () => {
     console.log("Menu-Big knop 3 is clicked");
-    bluezones.style.backgroundImage = "url(images/1_basis.gif)";
+    bluezone2.style.backgroundImage = "url(images/1_basis.gif)";
     mainKnop3.style.zIndex = 200;
     mainKnop2.style.zIndex = 200;
     mainKnop.style.zIndex = 201;
-
-    image1.attr('src', image1.src);
+ 
 })
