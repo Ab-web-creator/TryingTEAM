6b9d70b001532bb8cbe57379fdd8b5ec05c7c66a
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-09-18 16:22:35 +0200

    lyuboe sobwenie

--- js/homePageOnly.js
+++ js/homePageOnly.js
@@ -11,6 +11,10 @@ dynamicContainer.appendChild(pageLogo)
 dynamicContainer.appendChild(pageHeader)
 
 const blueZoneContainer = document.querySelector('.blueZoneContainer')
+const basis = document.querySelector('.basis')
+const start = document.querySelector('.start')
+const stop = document.querySelector('.stop')
+
 const blueZoneImage = document.querySelector('.blueZoneImage')
 
 const page1 = document.getElementById('page-1')
@@ -27,60 +31,78 @@ let sectionTwoView = false
 let sectionThreeView = false
 let sectionFourView = false
 
-let basisMenuUrlPage1 = `./images/1_basis.gif`
-let startMenuUrlPage1 = `./images/1_start_menu.gif`
-let stopMenuUrlPage1 = `./images/1_stop_menu.gif`
+let basisMenuUrlPage1 = `./images/1_basis.gif?a=${Math.random()}`
 
 let basisMenuUrlPage2 = `./images/2_basis.gif?a=${Math.random()}`
-let startMenuUrlPage2 = `./images/2_start_menu.gif?a=${Math.random()}`
-let stopMenuUrlPage2
 
 let basisMenuUrlPage3 = `./images/3_basis.gif?a=${Math.random()}`
-let startMenuUrlPage3 = `./images/3_start_menu.gif?a=${Math.random()}`
-let stopMenuUrlPage3
 
 let basisMenuUrlPage4 = `./images/4_basis.gif?a=${Math.random()}`
-let startMenuUrlPage4 = `./images/4_start_menu.gif?a=${Math.random()}`
-let stopMenuUrlPage4
 
 const mainButton = document.querySelector('.mainButton')
 mainButton.addEventListener('click', () => {
   if (sectionOneView) {
+    let startMenuUrlPage1 = `./images/1_start_menu.gif?a=${Math.random()}`
+    let stopMenuUrlPage1 = `./images/1_stop_menu.gif?a=${Math.random()}`
     if (basisImageDisplay) {
-      blueZoneImage.src = startMenuUrlPage1
+      start.src = startMenuUrlPage1
+      setTimeout(() => {
+        start.style.zIndex = 2
+        stop.style.zIndex = 1
+        basis.style.zIndex = 0
+      }, 300)
 
       basisImageDisplay = false
-      // starImageDisplay = true
+
       mainButton.disabled = true
       setTimeout(() => {
         mainButton.disabled = false
-      }, 2600)
+      }, 2450)
     } else {
-      blueZoneImage.src = stopMenuUrlPage1
+      stop.src = stopMenuUrlPage1
+      setTimeout(() => {
+        stop.style.zIndex = 2
+        start.style.zIndex = 1
+        basis.style.zIndex = 0
+      }, 500)
 
       basisImageDisplay = true
+
       mainButton.disabled = true
       setTimeout(() => {
         mainButton.disabled = false
-      }, 2600)
+      }, 2450)
     }
   }
 
   if (sectionTwoView) {
     console.log('we are clicking on section two')
+    let startMenuUrlPage2 = `./images/2_start_menu.gif?a=${Math.random()}`
+    let stopMenuUrlPage2 = `./images/2_stop_menu.gif?a=${Math.random()}`
     if (basisImageDisplay) {
-      blueZoneImage.src = startMenuUrlPage2
+      start.src = startMenuUrlPage2
+      setTimeout(() => {
+        start.style.zIndex = 2
+        stop.style.zIndex = 1
+        basis.style.zIndex = 0
+      }, 300)
+
       basisImageDisplay = false
-      starImageDisplay = true
+
       mainButton.disabled = true
       setTimeout(() => {
         mainButton.disabled = false
       }, 2450)
-
-      stopMenuUrlPage2 = `./images/2_stop_menu.gif?a=${Math.random()}`
     } else {
-      blueZoneImage.src = stopMenuUrlPage2
+      stop.src = stopMenuUrlPage2
+      setTimeout(() => {
+        stop.style.zIndex = 2
+        start.style.zIndex = 1
+        basis.style.zIndex = 0
+      }, 500)
+
       basisImageDisplay = true
+
       mainButton.disabled = true
       setTimeout(() => {
         mainButton.disabled = false
@@ -90,19 +112,32 @@ mainButton.addEventListener('click', () => {
 
   if (sectionThreeView) {
     console.log('we are clicking on section three')
+    let startMenuUrlPage3 = `./images/3_start_menu.gif?a=${Math.random()}`
+    let stopMenuUrlPage3 = `./images/3_stop_menu.gif?a=${Math.random()}`
     if (basisImageDisplay) {
-      blueZoneImage.src = startMenuUrlPage3
+      start.src = startMenuUrlPage3
+      setTimeout(() => {
+        start.style.zIndex = 2
+        stop.style.zIndex = 1
+        basis.style.zIndex = 0
+      }, 300)
+
       basisImageDisplay = false
-      starImageDisplay = true
+
       mainButton.disabled = true
       setTimeout(() => {
         mainButton.disabled = false
       }, 2450)
-
-      stopMenuUrlPage3 = `./images/3_stop_menu.gif?a=${Math.random()}`
     } else {
-      blueZoneImage.src = stopMenuUrlPage3
+      stop.src = stopMenuUrlPage3
+      setTimeout(() => {
+        stop.style.zIndex = 2
+        start.style.zIndex = 1
+        basis.style.zIndex = 0
+      }, 500)
+
       basisImageDisplay = true
+
       mainButton.disabled = true
       setTimeout(() => {
         mainButton.disabled = false
@@ -112,19 +147,32 @@ mainButton.addEventListener('click', () => {
 
   if (sectionFourView) {
     console.log('we are clicking on section four')
+    let startMenuUrlPage4 = `./images/4_start_menu.gif?a=${Math.random()}`
+    let stopMenuUrlPage4 = `./images/4_stop_menu.gif?a=${Math.random()}`
     if (basisImageDisplay) {
-      blueZoneImage.src = startMenuUrlPage4
+      start.src = startMenuUrlPage4
+      setTimeout(() => {
+        start.style.zIndex = 2
+        stop.style.zIndex = 1
+        basis.style.zIndex = 0
+      }, 300)
+
       basisImageDisplay = false
-      starImageDisplay = true
+
       mainButton.disabled = true
       setTimeout(() => {
         mainButton.disabled = false
       }, 2450)
-
-      stopMenuUrlPage4 = `./images/4_stop_menu.gif?a=${Math.random()}`
     } else {
-      blueZoneImage.src = stopMenuUrlPage4
+      stop.src = stopMenuUrlPage4
+      setTimeout(() => {
+        stop.style.zIndex = 2
+        start.style.zIndex = 1
+        basis.style.zIndex = 0
+      }, 500)
+
       basisImageDisplay = true
+
       mainButton.disabled = true
       setTimeout(() => {
         mainButton.disabled = false
@@ -135,6 +183,8 @@ mainButton.addEventListener('click', () => {
 
 mainDiv.addEventListener('scroll', () => {
   const scrolledHeight = mainDiv.scrollTop
+
+  basisMenuUrlPage1 = `./images/1_basis.gif?a=${Math.random()}`
 
   if (scrolledHeight >= 0 && scrolledHeight <= page1.scrollHeight * 0.1) {
     dynamicContainer.style.opacity = 1
@@ -182,7 +232,9 @@ mainDiv.addEventListener('scroll', () => {
     pageHeader.innerText = 'De rijopleiding'
 
     basisImageDisplay = true
-    blueZoneImage.src = basisMenuUrlPage1
+
+    basis.src = basisMenuUrlPage1
+    basis.style.zIndex = 2
 
     console.log('mid top 1-2')
     sectionOneView = true
@@ -201,7 +253,9 @@ mainDiv.addEventListener('scroll', () => {
     pageHeader.innerText = 'De simulator'
 
     basisImageDisplay = true
-    blueZoneImage.src = basisMenuUrlPage2
+
+    basis.src = basisMenuUrlPage2
+    basis.style.zIndex = 2
 
     sectionOneView = false
     sectionTwoView = true
@@ -290,7 +344,9 @@ mainDiv.addEventListener('scroll', () => {
     console.log('mid top 2-3')
 
     basisImageDisplay = true
-    blueZoneImage.src = basisMenuUrlPage2
+
+    basis.src = basisMenuUrlPage2
+    basis.style.zIndex = 2
 
     sectionOneView = false
     sectionTwoView = true
@@ -307,7 +363,9 @@ mainDiv.addEventListener('scroll', () => {
 
     console.log('mid bottom 2-3')
     basisImageDisplay = true
-    blueZoneImage.src = basisMenuUrlPage3
+
+    basis.src = basisMenuUrlPage3
+    basis.style.zIndex = 2
 
     sectionOneView = false
     sectionTwoView = false
@@ -407,7 +465,9 @@ mainDiv.addEventListener('scroll', () => {
     console.log('mid top 3-4')
 
     basisImageDisplay = true
-    blueZoneImage.src = basisMenuUrlPage3
+
+    basis.src = basisMenuUrlPage3
+    basis.style.zIndex = 2
 
     sectionOneView = false
     sectionTwoView = false
@@ -425,7 +485,9 @@ mainDiv.addEventListener('scroll', () => {
     pageHeader.innerText = 'De impact'
 
     basisImageDisplay = true
-    blueZoneImage.src = basisMenuUrlPage4
+
+    basis.src = basisMenuUrlPage4
+    basis.style.zIndex = 2
 
     sectionOneView = false
     sectionTwoView = false
