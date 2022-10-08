60b307744b4089769dd655fce829729391f658de
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-10-08 21:11:57 +0200

    lyuboe sobwenie

--- js/homePageOnly.js
+++ js/homePageOnly.js
@@ -36,6 +36,76 @@ let basisMenuUrlPage2 = `./images/2_basis.gif?a=${Math.random()}`
 let basisMenuUrlPage3 = `./images/3_basis.gif?a=${Math.random()}`
 let basisMenuUrlPage4 = `./images/4_basis.gif?a=${Math.random()}`
 
+//Abeke added - started here
+
+const linkButton1 = document.querySelector('.linkButton1')
+linkButton1.addEventListener('click', () => {
+  if (sectionOneView) {
+    window.open('html_pages/opbouw.html', '_self');
+  }
+  if (sectionTwoView) {
+    window.open('html_pages/doelen.html', '_self');
+  }
+  if (sectionThreeView) {
+    window.open('html_pages/tontom.html', '_self');
+  }
+  if (sectionFourView) {
+    window.open('html_pages/wissers.html', '_self');
+  }
+})
+
+const linkButton2 = document.querySelector('.linkButton2')
+linkButton2.addEventListener('click', () => {
+  if (sectionOneView) {
+    window.open('html_pages/doelen.html', '_self');
+  }
+  if (sectionTwoView) {
+    window.open('html_pages/tarieven.html', '_self');
+  }
+  if (sectionThreeView) {
+    window.open('html_pages/tontom.html', '_self');
+  }
+  if (sectionFourView) {
+    window.open('html_pages/wissers.html', '_self');
+  }
+})
+
+const linkButton3 = document.querySelector('.linkButton3')
+linkButton3.addEventListener('click', () => {
+  if (sectionOneView) {
+    window.open('html_pages/tontom.html', '_self');
+  }
+  if (sectionTwoView) {
+    window.open('html_pages/doelen.html', '_self');
+  }
+  if (sectionThreeView) {
+    window.open('html_pages/opbouw.html', '_self');
+  }
+  if (sectionFourView) {
+    window.open('html_pages/wissers.html', '_self');
+  }
+})
+
+const linkButton4 = document.querySelector('.linkButton4')
+linkButton4.addEventListener('click', () => {
+  if (sectionOneView) {
+    window.open('html_pages/tontom.html', '_self');
+  }
+  if (sectionTwoView) {
+    window.open('html_pages/doelen.html', '_self');
+  }
+  if (sectionThreeView) {
+    window.open('html_pages/opbouw.html', '_self');
+  }
+  if (sectionFourView) {
+    window.open('html_pages/wissers.html', '_self');
+  }
+})
+
+
+
+//Abeke added - ended here
+
 const mainButton = document.querySelector('.mainButton')
 mainButton.addEventListener('click', () => {
   if (sectionOneView) {
